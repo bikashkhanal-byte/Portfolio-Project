87569ce29c59176ecf3cea1dfbbdedb98d1b6553
Bikash_Khanal <bikashkhanak760@gmail.com>
2025-01-05 20:58:19 +0545

    Add all files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function ShowSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex'
+}
+
+function HideSideBar(){
+     const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none'
+}
